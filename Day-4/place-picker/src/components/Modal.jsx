03ff276b39fc7e58e59d9f320d2d3dc18a56933b
Ajay <ajayyasodha@gmail.com>
2024-05-27 18:49:23 +0530

    Added completed placer picker project exploring useEffect hook

---
+++ Day-4/place-picker/src/components/Modal.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useRef } from "react";
+import { createPortal } from "react-dom";
+
+function Modal({ open, children, onClose }) {
+  const dialog = useRef();
+
+  useEffect(
+    () => (open ? dialog.current.showModal() : dialog.current.close()),
+    [open]
+  );
+
+  return createPortal(
+    <dialog className="modal" ref={dialog} onClose={onClose}>
+      {open && children}
+    </dialog>,
+    document.getElementById("modal")
+  );
+}
+
+export default Modal;

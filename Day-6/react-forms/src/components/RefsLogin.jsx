e6ef8720eee9b8f5d985e6b70f41c18e612cd877
Ajay <ajayyasodha@gmail.com>
2024-05-29 19:08:20 +0530

    Added react forms project exploring form submission, validation with state, refs, inbuilt form data and extracting state management and validation logic into a seperate hook

---
+++ Day-6/react-forms/src/components/RefsLogin.jsx
@@ -0,0 +1,58 @@
+import { useRef, useState } from "react";
+
+/* ----------- Handling with refs ----------- */
+
+export default function Login() {
+  const [isEmailInvalid, setIsEmailInvalid] = useState(false);
+  const email = useRef();
+  const password = useRef();
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    const enteredEmail = email.current.value;
+    const enteredPassword = password.current.value;
+
+    const isEmailValid = enteredEmail.includes("@");
+
+    if (!isEmailValid) {
+      setIsEmailInvalid(true);
+      return;
+    }
+
+    setIsEmailInvalid(false);
+
+    console.log(enteredEmail, enteredPassword);
+
+    // ! Imperative Approach - should be avoided
+    // email.current.value = ""
+    // password.current.value = ""
+
+    event.target.reset();
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <div className="control no-margin">
+          <label htmlFor="email">Email</label>
+          <input id="email" type="email" name="email" ref={email} />
+          <div className="control-error">
+            {isEmailInvalid && <p>Please enter a valid email address</p>}
+          </div>
+        </div>
+
+        <div className="control no-margin">
+          <label htmlFor="password">Password</label>
+          <input id="password" type="password" name="password" ref={password} />
+        </div>
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
---
+++ Day-6/react-forms/src/components/StateLogin.jsx
@@ -0,0 +1,66 @@
+import Input from "./Input";
+import { hasMinLength, isEmail, isNotEmpty } from "../util/validation";
+import { useInput } from "../hooks/useInput";
+
+/* ----------- Handling with state ----------- */
+
+export default function StateLogin() {
+  const {
+    value: emailValue,
+    handleInputChange: handleEmailChange,
+    handleBlur: handleEmailBlur,
+    hasError: emailHasError,
+  } = useInput("", (value) => isEmail(value) && isNotEmpty(value));
+
+  const {
+    value: passwordValue,
+    handleInputChange: handlePasswordChange,
+    handleBlur: handlePasswordBlur,
+    hasError: passwordHasError,
+  } = useInput("", (value) => hasMinLength(value, 6));
+
+  function handleSubmit(event) {
+    event.preventDefault();
+
+    if (emailHasError || passwordHasError) {
+      return;
+    }
+
+    console.log(emailValue, passwordValue);
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Login</h2>
+
+      <div className="control-row">
+        <Input
+          id="email"
+          label="Email"
+          type="email"
+          name="email"
+          onBlur={handleEmailBlur}
+          onChange={handleEmailChange}
+          value={emailValue}
+          error={emailHasError && "Please enter a valid email"}
+        />
+
+        <Input
+          id="password"
+          label="password"
+          type="password"
+          name="password"
+          onBlur={handlePasswordBlur}
+          onChange={handlePasswordChange}
+          value={passwordValue}
+          error={passwordHasError && "Please enter a valid password"}
+        />
+      </div>
+
+      <p className="form-actions">
+        <button className="button button-flat">Reset</button>
+        <button className="button">Login</button>
+      </p>
+    </form>
+  );
+}
